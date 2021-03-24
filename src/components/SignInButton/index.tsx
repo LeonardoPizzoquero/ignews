e98176dd58787826ab76e6a40e55dddc788c554f
Leonardo Pizzoquero <leonardopizzoquero@pecege.com>
2021-03-23 23:31:11 -0300

    Initial commit

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,21 @@
-import styles from './styles.module.scss';
 import { FiX } from 'react-icons/fi';
 import { FaGithub } from 'react-icons/fa';
+import { signIn, useSession, signOut } from 'next-auth/client'
+
+import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ?  (
+  return session ?  (
       <button 
         type="button"
         className={styles.signInButton}
+        onClick={() => signOut()}
       >
         <FaGithub color="#04d361" />
         
-        Leonardo Pizzoquero
+        {session.user.name}
 
         <FiX color="#737380" className={styles.closeIcon} />
       </button> 
@@ -20,6 +23,7 @@ export function SignInButton() {
       <button 
         type="button"
         className={styles.signInButton}
+        onClick={() => signIn('github')}
       >
         <FaGithub color="#eba417" />
         
